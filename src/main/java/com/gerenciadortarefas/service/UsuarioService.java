9f30dc0ddb2edb8da3cb493f294da541e084f255
Luan Valente <luanbvalente@hotmail.com>
2024-03-16 14:07:34 -0300

    Finalização das configurações de segurança

--- src/main/java/com/gerenciadortarefas/service/UsuarioService.java
+++ src/main/java/com/gerenciadortarefas/service/UsuarioService.java
@@ -1,28 +1,46 @@
 package com.gerenciadortarefas.service;
 
 import com.gerenciadortarefas.entity.users.Usuario;
+import com.gerenciadortarefas.repository.RoleRepository;
 import com.gerenciadortarefas.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     public Usuario salvarUsuario(Usuario usuario) {
+        usuario.setRoles(
+                usuario.getRoles()
+                        .stream()
+                        .map(role -> roleRepository.findByNome(role.getNome()))
+                        .toList());
+
         usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
         return usuarioRepository.save(usuario);
     }
 
     public Usuario atualizarUsuario(Usuario usuario) {
+        usuario.setRoles(
+                usuario.getRoles()
+                        .stream()
+                        .map(role -> roleRepository.findByNome(role.getNome()))
+                        .toList());
+
         usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
         return usuarioRepository.save(usuario);
     }
